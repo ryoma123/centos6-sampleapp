df8a848a3f49bb82dab5cce229f5dd90f701ca36
Ryoma Ono <ryoma.ono.2661@gmail.com>
2014-12-18 12:09:48 +0900

    Rails アプリを動かすために、Ruby 2.1.5 をインストールする 同時に rbenv と ruby-build をインストールする

--- vagrant/init.pp
+++ vagrant/init.pp
@@ -54,3 +54,34 @@ file {
     mode   => 644,
     source => '/vagrant/templates/.bash_profile';
 }
+
+exec {
+  'rbenv':
+    user        => 'vagrant',
+    environment => 'HOME=/home/vagrant',
+    command     => 'git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv',
+    creates     => '/home/vagrant/.rbenv',
+    require     => Package['git'];
+
+  'ruby build':
+    user        => 'vagrant',
+    environment => 'HOME=/home/vagrant',
+    command     => 'git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build',
+    creates     => '/home/vagrant/.rbenv/plugins/ruby-build',
+    require     => [
+      Package['git'],
+      Exec['rbenv'],
+    ];
+
+  'Ruby install':
+    cwd         => '/home/vagrant',
+    user        => 'vagrant',
+    environment => 'HOME=/home/vagrant',
+    command     => "bash -c 'source /home/vagrant/.bash_profile ; makeopts=\"-j4\" configure_opts=\"--disable-install-doc\" rbenv install 2.1.5 ; rbenv global 2.1.5 ; rbenv rehash'",
+    timeout     => 1800,
+    require     => [
+      File['/home/vagrant/.bash_profile'],
+      Exec['ruby build'],
+      Exec['rbenv'],
+    ];
+}
